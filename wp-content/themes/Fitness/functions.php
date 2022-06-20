abbff65462650d239986bd7ee0f332f5f8626552
mchii <200535@student.glu.nl>
2022-06-20 20:07:16 +0200

    screenshot

--- wp-content/themes/Fitness/functions.php
+++ wp-content/themes/Fitness/functions.php
@@ -4,6 +4,5 @@ function mytheme_menu()
     register_nav_menu('mijn_custom_menu', __('Mijn Custom Menu'));
 }
 add_action('init', 'mytheme_menu');
-
 add_theme_support('post-thumbnails');
 add_image_size('custom-size', 220, 180);
